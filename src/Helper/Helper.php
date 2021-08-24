c9c70e42cb9dc449d2bdbd626f422289c8704cad
Andreas Fieger <fiedsch@ja-eh.at>
2021-08-24 13:18:01 +0200

    Minor Fixes

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -7,6 +7,7 @@ class Helper
     public static function cleanUpString($value): string
     {
         $value = str_replace('&#34;', '"', $value);
+        $value = str_replace('&#39;', "'", $value);
 
         return $value;
     }
--- src/JsonWidgetBundle.php
+++ src/JsonWidgetBundle.php
@@ -5,8 +5,6 @@ namespace Fiedsch\JsonWidgetBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Configures the coder bundle.
- *
  * @author Andreas Fieger
  */
 class JsonWidgetBundle extends Bundle
