66a3f19836b1736cd04e43e2add52cf1d3b2a517
Andreas Fieger <fiedsch@ja-eh.at>
2023-05-03 15:11:40 +0200

    Use fully qualified class name

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,7 @@
 <?php
+use Contao\WidgetJSON;
+use Contao\WidgetYAML;
 
-$GLOBALS['BE_FFL']['jsonWidget'] = 'WidgetJSON';
-$GLOBALS['BE_FFL']['yamlWidget'] = 'WidgetYAML';
+$GLOBALS['BE_FFL']['jsonWidget'] = WidgetJSON::class;
+$GLOBALS['BE_FFL']['yamlWidget'] = WidgetYAML::class;
+
